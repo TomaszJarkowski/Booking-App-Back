0efcb3b71c425c30aae8746dfcf362eafa568a02
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-07 23:19:19 +0200

    create auth middleware and feature delete user

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res
+        .status(401)
+        .json({ msg: "No authentucatuin token, autorization denied." });
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verified) {
+      return res
+        .status(401)
+        .json({ msg: "Token verification field, autorization denied." });
+    }
+
+    req.user = verified.id;
+    next();
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
+};
+
+module.exports = auth;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const auth = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
   try {
@@ -82,6 +83,39 @@ router.post("/login", async (req, res) => {
         email: user.email,
       },
     });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const delitedUsers = await User.findByIdAndDelete(req.user);
+    res.json(delitedUsers);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.json(false);
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verified) {
+      return res.json(false);
+    }
+
+    const user = await User.findById(verified.id);
+    if (!user) {
+      return res.json(false);
+    }
+
+    return res.json(true);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
