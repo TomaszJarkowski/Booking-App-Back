7ec641986b136aea9b2054e10895fb8ea5f414b4
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-07 17:04:35 +0200

    create /register route and create simple validation data

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,40 @@
 const router = require("express").Router();
+const User = require("../models/userModel");
 
-router.get("/test", (req, res) => {
-  res.send("Hello, world!");
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, passwordCheck, displayName } = req.body;
+
+    //validation error
+
+    if (!email || !password || !passwordCheck) {
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    }
+    if (password.length < 5) {
+      return res
+        .status(400)
+        .json({ msg: "The password needs to be at least 5 characters long." });
+    }
+    if (password != passwordCheck) {
+      return res
+        .status(400)
+        .json({ msg: "Enter the same password twice for verification." });
+    }
+
+    const existingUser = await User.find({ email: email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists." });
+    }
+
+    if (!displayName) {
+      displayName = email;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
