c37af574e4cad94abb25271d610564f127b503c1
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-07 15:54:53 +0200

    start build application, start server and connect with server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server has started on port ${PORT}`);
+});
+
+// set up mongoose
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
